5d3386813a395d33376926241d981acb35e6affa
kondo takeshi <chaspy@users.noreply.github.com>
2023-02-05 15:25:47 +0900

    今日花金！

--- src/Hanakintaro.tsx
+++ src/Hanakintaro.tsx
@@ -1,10 +1,12 @@
 import logo from './assets/hanakintaro.png'
+import Response from './Response'
 import './Hanakintaro.css'
 
 function Hanakintaro() {
   return (
     <div>
       <img src={logo} alt="hanakintaro logo"></img>
+      <Response />
       <button>今日花金？</button>
     </div>
   )
---
+++ src/Response.tsx
@@ -0,0 +1,5 @@
+function Response() {
+  return <div>今日は花金！</div>
+}
+
+export default Response
